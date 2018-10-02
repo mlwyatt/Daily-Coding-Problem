56f2b56628821556b910b4880d01f91c3bb77f60
Marcus Wyatt <mlwyatt2008@gmail.com>
2018-10-02 12:44:29 -0400

    fix #6

--- dcp_problems.rb
+++ dcp_problems.rb
@@ -10,7 +10,7 @@ def problem2(ar)
   end
 end
 
-# problem3
+# problem 3
 class Node
   attr_accessor :val, :left, :right
   def initialize(val,left=nil,right=nil)
@@ -36,4 +36,69 @@ def problem4(ar)
   (1..ar.size+1).each do |a|
     return a if ar.index(a).nil?
   end
+end
+
+# problem 6
+class LLNode
+  def initialize(val)
+    @val = val
+    @both = 0
+  end
+  def val
+    @val
+  end
+  def add_next(n_node)
+    @both = @both ^ n_node
+  end
+  def add_prev(p_node)
+    @both = @both ^ p_node
+  end
+  def next_node(p_node)
+    ObjectSpace._id2ref(@both ^ p_node)
+  end
+  def prev_node(n_node)
+    ObjectSpace._id2ref(@both ^ n_node)
+  end
+  def both
+    @both
+  end
+  def get_pointer
+    object_id
+  end
+end
+class XorLL
+  def initialize(node)
+    @root = node
+    @last = @root
+  end
+  def add(node)
+    @last.add_next(node.get_pointer)
+    node.add_prev(@last.get_pointer)
+    @last = node
+  end
+  def get(index)
+    node = @root
+    prev = 0
+    while index > 0 do
+      n_node = node.next_node(prev)
+      prev = node.get_pointer
+      node = n_node
+      index -= 1
+    end
+    return node
+  end
+  def print
+    node = @root
+    prev = 0
+    while node.is_a?(LLNode) do
+      puts "<- #{node.val} ->"
+      n_node = node.next_node(prev)
+      prev = node.get_pointer
+      node = n_node
+    end
+  end
+end
+
+def problem7(decode)
+  
 end
